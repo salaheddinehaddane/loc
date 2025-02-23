bab434d054d28c9d3f2e131a16ccf83e9949647c
salaheddinehaddane <salahhaddane@gmail.com>
2025-02-23 21:27:04 +0000

    create SubscriptionPlan Model

---
+++ app/Models/SubscriptionPlan.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SubscriptionPlan extends Model
+{
+     protected $fillable = [
+        'name',
+        'price',
+        'duration',
+        'max_cars',
+        'max_offers',
+        'is_active',
+        'features',
+        'description',
+     ];
+
+     protected $casts = [
+        'features' => 'array',
+     ];
+}
