3e422e6601b59d9157e67ff892722d186fb3703b
salaheddinehaddane <salahhaddane@gmail.com>
2025-02-23 21:27:44 +0000

    subscription plans migration

---
+++ database/migrations/2025_02_23_212054_create_subscription_plans_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('subscription_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('duration'); // in months
+            $table->integer('max_cars')->nullable();
+            $table->integer('max_offers')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->json('features')->nullable();
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        Schema::table('companies', function (Blueprint $table) {
+            $table->foreignId('subscription_plan_id')->references('id')->on('subscription_plans')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('subscription_plans');
+
+        Schema::table('companies', function (Blueprint $table) {
+            $table->dropForeign(['subscription_plan_id']);
+        });
+    }
+};
